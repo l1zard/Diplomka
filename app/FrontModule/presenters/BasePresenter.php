6832254a09ca97dca7d861b436bbfe165c59277e
l1zard <l1zardzor@gmail.com>
2016-03-29 09:46:08 +0200

    update
    update

--- app/FrontModule/presenters/BasePresenter.php
+++ app/FrontModule/presenters/BasePresenter.php
@@ -5,7 +5,7 @@ namespace App\FrontModule\Presenters;
 use Nette;
 use App\Model;
 use Nette\Application\UI;
-
+use Nette\Application\UI\Form;
 
 /**
  * Base presenter for all application presenters.
@@ -16,15 +16,35 @@ abstract class BasePresenter extends Nette\Application\UI\Presenter
 
     protected function createComponentLoginUser(){
         $form = new UI\Form();
-        $form->addText('username');
-        $form->addPassword('password');
-        $form->addSubmit('send');
+        $form->addText('username')
+            ->setAttribute('class', 'textinput')
+            ->setAttribute('placeholder', 'Uživatelské jméno')
+            ->addRule(Form::FILLED, 'Vyplňte prosím jméno');
+        $form->addPassword('password')
+            ->setAttribute('class', 'textinput')
+            ->setAttribute('placeholder', 'Heslo')
+            ->addRule(Form::FILLED, 'Vyplňte prosím heslo');
+        $form->addSubmit('send','Přihlásit')
+            ->setAttribute('class', 'button-blue');
         $form->onSuccess[] = $this->loginUserSucceed;
 
         return $form;
     }
 
-    public function loginUserSucceed(){
+    public function loginUserSucceed(Form $form){
+        $values = $form->values;
+        $this->user->setExpiration('20 minutes', TRUE);
+        try {
+            $this->user->login($values->username, $values->password);
+        } catch (Nette\Security\AuthenticationException $e) {
+            $form->addError($e->getMessage());
+        }
+    }
 
+    public function handleUserLogout(){
+        if($this->user->isLoggedIn()){
+            $this->user->logout();
+            $this->redirect('this');
+        }
     }
 }
