48678e5c7faf9a77e51e5216d233955ae230c92b
l1zard <l1zardzor@gmail.com>
2016-04-01 00:34:43 +0200

    update
    update

--- app/FrontModule/presenters/HomepagePresenter.php
+++ app/FrontModule/presenters/HomepagePresenter.php
@@ -6,43 +6,51 @@ use Nette;
 use App\Model;
 use App\Model\MatchModel;
 
-class HomepagePresenter extends BasePresenter
-{
-	
+class HomepagePresenter extends BasePresenter {
+
 	/** @var MatchModel @inject */
-    public $matchModel;
-	public function renderDefault()
-	{
+	public $matchModel;
+
+	public function renderDefault() {
 		$this->template->anyVariable = 'any value';
-		$this->template->matches = $this->matchModel->getIncommingMatchs();
-		$this->template->tiket  = $this->session->getSection('tiket');
-		
+		$this->template->matches     = $this->matchModel->getIncommingMatchs();
+		$this->template->tiket       = $this->session->getSection('tiket');
 	}
-	public function handleAddToTicket($idMatch, $idOppurtiny){
+
+	public function handleAddToTicket($idMatch, $idOppurtiny) {
 		$sekce   = $this->session->getSection('tiket');
 		$matches = $this->matchModel->getIncommingMatchs();
-		foreach($matches as $match){
-			if($match->id_zapas == $idMatch){
+		foreach($matches as $match) {
+			if($match->id_zapas == $idMatch) {
 				$zapas = $match;
 			}
 		}
-		foreach($zapas->prilezitosti as $oppurtunity){
-			if($oppurtunity->id_prilezitost == $idOppurtiny){
+		foreach($zapas->prilezitosti as $oppurtunity) {
+			if($oppurtunity->id_prilezitost == $idOppurtiny) {
 				$prilezitost = $oppurtunity;
 			}
 		}
+
 		$sekce->polozka[$idMatch] = array(
-			"zapas" => $zapas->klubdomaci . '-' . $zapas->klubhoste, 
-			"kurz"  => $prilezitost['kurz'],
-			"prilezitost"  => $this->matchModel->getTypPrilezitostString($prilezitost->id_typ_prilezitosti),
-			"id"    => $idOppurtiny,
-			"idMatch"    => $idMatch
+			"zapas"       => $zapas->klubdomaci . '-' . $zapas->klubhoste,
+			"kurz"        => $prilezitost['kurz'],
+			"prilezitost" => $this->matchModel->getTypPrilezitostString($prilezitost->id_typ_prilezitosti),
+			"id"          => $idOppurtiny,
+			"idMatch"     => $idMatch
 		);
+
+		$sekce->celkovyKurz = 1;
+
+		foreach($sekce->polozka as $item) {
+			$sekce->celkovyKurz = $sekce->celkovyKurz * $item['kurz'];
+
+		}
+
 		$this->redrawControl('ticketSnippet');
 	}
-	
-	public function handleDeleteMatchFromTicket($idMatch){
-		$sekce   = $this->session->getSection('tiket');
+
+	public function handleDeleteMatchFromTicket($idMatch) {
+		$sekce = $this->session->getSection('tiket');
 		unset($sekce->polozka[$idMatch]);
 		$this->redrawControl('ticketSnippet');
 	}
